2ff1a7b986e1d92a39b3447584d5f465803a449f
craig.warner <cgwarner2014@gmail.com>
2023-06-10 10:26:40 -0500

    addded setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mandelbrot-background", #
+    version="1.0.0",
+    author="Craig Warner",
+    author_email="cgwarner2014@gmail.com",
+    description="Mandelbrot Background",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/craig-warner/MandelbrotBackground",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    scripts=['bin/mandelbrot-background'],
+)
