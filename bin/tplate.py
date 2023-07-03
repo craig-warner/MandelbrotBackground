189700b4251d570f2b1185054eca88d12e7e2716
craig.warner <cgwarner2014@gmail.com>
2023-07-03 08:46:23 -0500

    featurre: image control

--- bin/tplate.py
+++ bin/tplate.py
@@ -1,19 +1,27 @@
 class Tplate:
     def __init__(self):
-        self.default = 1
+        self.default = 4
         self.descriptions = [
+            "Big Center (17 images)",
+            "Diagonal (17 images)",
+            "Five (5 images)",
             "Left to Right, Enlarge, (3 images)",
             "Left to Right, Enlarge, (8 images)",
             "Left to Right, Enlarge, (11 images)",
             "Left to Right, Enlarge, (14 images)",
-            "Big Center, (14 images)"
+            "One (1 image)",
+            "Two (2 images)"
         ]
         self.filename= [
+            "center.json",
+            "diagonal.hjson",
+            "five.hjson",
             "three_with_comments.hjson",
             "eight.json",
             "eleven.json",
             "fourteen.json",
-            "center.json"
+            "one.hjson",
+            "two.hjson"
         ] 
 
     def GetAllTemplates(self):
